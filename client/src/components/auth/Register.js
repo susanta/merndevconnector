a51c8d37af9dc3cb042d9e78527b207de8c9e273
Susanta <inbox.susanta@gmail.com>
2020-04-21 21:36:02 +0530

    Login Form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -22,29 +22,7 @@ const Register = () => {
     if (password !== password2) {
       console.log('Password do not match');
     } else {
-      // console.log(formData);
-      const newUser = {
-        name,
-        email,
-        password,
-        password2,
-      };
-
-      try {
-        const config = {
-          headers: {
-            'Content-type': 'application/json',
-          },
-        };
-
-        const body = JSON.stringify(newUser);
-
-        const res = await axios.post('/api/users', body, config);
-
-        console.log(res.data);
-      } catch (err) {
-        console.log(err.response.data);
-      }
+      console.log('Success');
     }
   };
 
@@ -102,7 +80,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
