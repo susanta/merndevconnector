102dd5cc94b3a3bf9a455be689e170cca4d775da
Susanta <inbox.susanta@gmail.com>
2020-04-23 22:25:12 +0530

    Load user & set auth token

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -11,7 +16,14 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case 'REGISTER_SUCCESS':
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
+    case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -19,7 +31,8 @@ export default function (state = initialState, action) {
         isAuthenticated: true,
         loading: false,
       };
-    case 'REGISTER_FAIL':
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
