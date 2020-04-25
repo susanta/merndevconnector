7f85986bd8a1957112edbca3f739cbc1adc253a6
Susanta <inbox.susanta@gmail.com>
2020-04-25 21:30:08 +0530

    Edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -40,6 +41,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               />
             </Switch>
           </section>
